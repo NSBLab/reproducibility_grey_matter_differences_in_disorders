2fe3a79be266ec3fae511f23e2ebb9dc996240b7
Trang Cao <Trang.Cao@monash.edu>
2025-09-16 17:02:24 +1000

    fix pipeline

--- run_pipeline.m
+++ run_pipeline.m
@@ -207,6 +207,22 @@ end
 success = true;
 end
 
+function enabled_datasets = get_enabled_datasets(config)
+% Return cell array of dataset names with enabled == true
+enabled_datasets = {};
+if ~isfield(config, 'datasets') || isempty(fieldnames(config.datasets))
+    return;
+end
+dataset_names = fieldnames(config.datasets);
+for k = 1:numel(dataset_names)
+    name_k = dataset_names{k};
+    ds = config.datasets.(name_k);
+    if isfield(ds, 'enabled') && logical(ds.enabled)
+        enabled_datasets{end+1} = name_k; %#ok<AGROW>
+    end
+end
+end
+
 % --- VBM steps ---
 
 function success = run_vbm_cat12(config)
